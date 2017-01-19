fc495e056b40d8a8f477ba046caf1e451b0b1305
Matt <restore.technical@gmail.com>
2017-01-19 22:04:37 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * The template for displaying comments
+ * changed
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
  *
